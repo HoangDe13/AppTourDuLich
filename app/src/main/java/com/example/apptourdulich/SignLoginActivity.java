145f3ef5f31b577306608d1a6b14919bcdc90c85
ASUS <huynh13092000@gmail.com>
2021-05-06 10:03:57 +0700

    Xử lí đăng nhập facebook

--- app/src/main/java/com/example/apptourdulich/SignLoginActivity.java
+++ app/src/main/java/com/example/apptourdulich/SignLoginActivity.java
@@ -5,9 +5,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.media.MediaPlayer;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +38,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class SignLoginActivity extends AppCompatActivity {
