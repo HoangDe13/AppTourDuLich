7475c5289f367b81031d7dc30fd9cccf5937cebd
ASUS <huynh13092000@gmail.com>
2021-05-18 14:48:53 +0700

    giao diện liên hệ , điều khoản

---
+++ app/src/main/java/com/example/apptourdulich/Rules.java
@@ -0,0 +1,14 @@
+package com.example.apptourdulich;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rules extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
